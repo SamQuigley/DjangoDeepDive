3caf1a1b5f774a217369bea0e1de19861b43a794
samquigley <samq2011@live.com>
2018-02-11 13:03:21 +0000

    starting blog app

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request,  'personal/basic.html', { 'content':['if you would like to contact me pls email me','sam@eventor.com']})
